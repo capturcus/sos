d0496fb8715da1555648fd086c9275cb41516f26
capturcus@sardynka <mp347235@students.mimuw.edu.pl>
2015-02-18 22:10:36 +0100

    added basic functionality to server uithread

--- sos-qtserver/uithread.cpp
+++ sos-qtserver/uithread.cpp
@@ -1,5 +1,8 @@
 ﻿#include "uithread.h"
 #include <QtCore\qdebug.h>
+#include <string>
+#include <iostream>
+#include <QtWidgets\qapplication.h>
 
 UIThread::UIThread(QObject *parent)
 	: QThread(parent)
@@ -14,5 +17,16 @@ UIThread::~UIThread()
 
 void UIThread::run() {
 	qDebug() << "Server started.";
-	QString line;
+	std::string line;
+	while (true)
+	{
+		std::getline(std::cin, line);
+		if (line == "exit")
+		{
+			qApp->exit();
+		}
+		else {
+			std::cout << "Unknown command: " << line << "\n";
+		}
+	}
 }
